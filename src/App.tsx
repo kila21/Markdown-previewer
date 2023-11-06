858e21301b4361c268b32dd3ce463a7ed4ba9b37
kila <luka.kilasonia.1@btu.edu.ge>
2023-11-06 19:29:13 +0400

    project init and icons install

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="editor-container">
+        <div className="editor-toolbar">
+          <p>Editor</p>
+          <i className="fa fa-arrows-alt" aria-hidden="true"></i>
+        </div>
+      </div>
+      <div className="preview-container"></div>
+    </>
+  );
+}
+
+export default App;
