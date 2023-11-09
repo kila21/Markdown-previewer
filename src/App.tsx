312d53dbd5556c180c2199fd8a84e2bbed4e2f60
kila <luka.kilasonia.1@btu.edu.ge>
2023-11-09 16:32:34 +0400

    parse and display text as html text

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+import { marked } from "marked";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+
   return (
     <>
       <div className="editor-container">
         <div className="editor-toolbar">
-          <p>Editor</p>
+          Editor
           <i className="fa fa-arrows-alt" aria-hidden="true"></i>
         </div>
+        <textarea
+          id="editor"
+          onChange={(e) => setText(marked.parse(e.target.value))}
+        ></textarea>
       </div>
-      <div className="preview-container"></div>
+      <div className="preview-container">
+        <div className="preview-toolbar">
+          Previewer
+          <i className="fa fa-arrows-alt" aria-hidden="true"></i>
+        </div>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(text) }}
+        ></div>
+      </div>
     </>
   );
 }
