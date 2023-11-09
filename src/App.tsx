888c5f58259f79f9bb713f73f2a41db86b8ec161
kila <luka.kilasonia.1@btu.edu.ge>
2023-11-09 17:07:06 +0400

    add resize functions

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,63 @@ function App() {
   const [text, setText] = useState(
     "You Can type anything here, and use any tag from html!."
   );
+  const [editor, setEditor] = useState(true);
+  const [preview, setPreview] = useState(true);
 
   return (
     <>
-      <div className="editor-container">
-        <div className="editor-toolbar">
-          Editor
-          <i className="fa fa-arrows-alt" aria-hidden="true"></i>
+      {editor && (
+        <div className="editor-container">
+          <div className="editor-toolbar">
+            Editor
+            {editor && preview && (
+              <i
+                onClick={() => setPreview(false)}
+                className="fa fa-arrows-alt"
+                aria-hidden="true"
+              ></i>
+            )}
+            {editor && !preview && (
+              <i
+                onClick={() => setPreview(true)}
+                className="fa fa-compress"
+                aria-hidden="true"
+              ></i>
+            )}
+          </div>
+          <textarea
+            className={!preview ? "resize" : ""}
+            id="editor"
+            onChange={(e) => setText(marked.parse(e.target.value))}
+          ></textarea>
         </div>
-        <textarea
-          id="editor"
-          onChange={(e) => setText(marked.parse(e.target.value))}
-        ></textarea>
-      </div>
-      <div className="preview-container">
-        <div className="preview-toolbar">
-          Previewer
-          <i className="fa fa-arrows-alt" aria-hidden="true"></i>
+      )}
+      {preview && (
+        <div className="preview-container">
+          <div className="preview-toolbar">
+            Previewer
+            {preview && editor && (
+              <i
+                onClick={() => setEditor(false)}
+                className="fa fa-arrows-alt"
+                aria-hidden="true"
+              ></i>
+            )}
+            {preview && !editor && (
+              <i
+                onClick={() => setEditor(true)}
+                className="fa fa-compress"
+                aria-hidden="true"
+              ></i>
+            )}
+          </div>
+          <div
+            className={!editor ? "resize" : ""}
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: marked(text) }}
+          ></div>
         </div>
-        <div
-          id="preview"
-          dangerouslySetInnerHTML={{ __html: marked(text) }}
-        ></div>
-      </div>
+      )}
     </>
   );
 }
